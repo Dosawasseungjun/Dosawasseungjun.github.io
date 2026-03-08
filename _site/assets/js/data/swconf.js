9a1afb68cabbefde8db2551e7e462799e006dd95
dosawas <dosawas@naver.com>
2026-03-08 14:49:32 +0900

    post in my mac

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772514624',resources: [
+    cacheName: 'chirpy-1772948819',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
